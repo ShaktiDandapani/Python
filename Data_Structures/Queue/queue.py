4864221226db3144912b5243471beedeb7056d82
Shaktidhar Dandapani <shaktidhar_dandapani@hotmail.com>
2019-12-26 19:14:24 +0000

    Added a bit of initial code for queue

---
+++ Data_Structures/Queue/queue.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+class Queue():
+
+	def __init__(size):
+
+		pass 
+
+	def enqueue(item):
+		"""
+		Add item to the queue from the rear.
+		"""
+
+		pass 
+
+	def dequeue():
+		"""
+		Remove item from the queue,
+		as it was inserted, i.e. 
+		from the front of the queue.
+		"""
+
+		pass 
+
+	def front(item):
+		"""
+		Get the front item in the queue
+		"""
+
+		pass 
+
+	def rear(item):
+				"""
+		Get the last item in the queue
+		"""
+
+		pass 
