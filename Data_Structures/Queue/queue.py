65ce387b206f1cdf0b8d04191ba33422d0d8054a
Shaktidhar Dandapani <shaktidhar_dandapani@hotmail.com>
2019-12-29 19:49:39 +0000

    Cleaning merge messages

--- Data_Structures/Queue/queue.py
+++ Data_Structures/Queue/queue.py
@@ -3,16 +3,12 @@
 
 class Queue():
 
-<<<<<<< HEAD
-	def __init__(self, size):
-=======
 	def __init__(size):
->>>>>>> a17c8c67296eaab636eb52ff773c85be1ebcc73a
+
 		self.queue_array = [None for x in range(0, size)]
 		self.front = -1
 		self.rear  = 0
 
-<<<<<<< HEAD
 	def enqueue(self, item):
 		"""
 		Add item to the queue from the rear.
@@ -21,42 +17,25 @@ class Queue():
 		self.queue_array[self.front] = item
 		print("Item {} added to the queue".format(item))
 
-
-	def dequeue(self):
-=======
-		pass 
-
-	def enqueue(item):
-		"""
-		Add item to the queue from the rear.
-		"""
-
-		pass 
-
 	def dequeue():
->>>>>>> a17c8c67296eaab636eb52ff773c85be1ebcc73a
+
 		"""
 		Remove item from the queue,
 		as it was inserted, i.e. 
 		from the front of the queue.
 		"""
-<<<<<<< HEAD
+
 		self.queue_array.pop(self.front)
 		self.front -= 1
-		 
-	def front(self, item):
-=======
 
-		pass 
 
 	def front(item):
->>>>>>> a17c8c67296eaab636eb52ff773c85be1ebcc73a
+
 		"""
 		Get the front item in the queue
 		"""
 
-<<<<<<< HEAD
-		return self.queue_array[self.front] 
+	return self.queue_array[self.front] 
 
 	def rear(self, item):
 		"""
@@ -64,24 +43,12 @@ class Queue():
 		"""
 		return self.queue_array[self.rear]
 
-	def is_full(self):
-=======
-		pass 
-
-	def rear(item):
-				"""
-		Get the last item in the queue
-		"""
-
-		pass 
-
 	def is_full():
->>>>>>> a17c8c67296eaab636eb52ff773c85be1ebcc73a
+
 		"""
 		Return True/ False based on whether the queue
 		is full/not full.
 		"""
-<<<<<<< HEAD
 		if None in self.queue_array:
 			print("The queue is not full")
 			return False
@@ -89,18 +56,11 @@ class Queue():
 			print("The queue is full")
 			return True
 
-	def is_empty(self):
-=======
-
-		pass 
-
 	def is_empty():
->>>>>>> a17c8c67296eaab636eb52ff773c85be1ebcc73a
 		"""
 		Return True/ False based on whether the queue
 		is empty/not empty.
 		"""
-<<<<<<< HEAD
 		empty_bool = True
 
 		# First check is to see if the queue is an 
@@ -115,7 +75,3 @@ class Queue():
 					return empty_bool
 		else:
 			return empty_bool
-
-=======
-		pass 
->>>>>>> a17c8c67296eaab636eb52ff773c85be1ebcc73a
